32775b09145e6c33d6b94e00199a3808d700ae74
Kushagra Bharti <kushagrabharti04@gmail.com>
2026-09-20 13:08:17 +0530

    feat: add database migration setup and create listings table schema

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: build run clean
+.PHONY: build run clean 
 
 build:
 	@go build -o bin/api ./cmd/api/main.go
@@ -7,4 +7,10 @@ run: build
 	@./bin/api
 
 clean:
-	@rm bin/api 
+	@rm bin/api
+
+migrate-up: # used to apply migration
+	@go run ./cmd/migrate up
+
+migrate-down: # used to undo migration
+	@go run ./cmd/migrate down
---
+++ cmd/migrate/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/kushagra/olx-api/internal/config"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+// migration means :- jo maine sql file bnayi hai usse apne databse ke upr apply krna.
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("please provide migration name <up | down>")
+		return
+	}
+
+	cnf := config.MustLoad()
+
+	// database se connect krna hai
+	m, err := migrate.New(
+		"file://migrations", // migration folder ka path
+		cnf.DatabaseUrl,
+	)
+
+	if err != nil {
+		log.Fatalf("error while creating migration obj: %v", err)
+	}
+
+	// switch case for up and down
+	switch os.Args[1] {
+	case "up":
+		fmt.Println("Running migration....!")
+		err = m.Up()
+		if err != nil {
+			log.Fatalf("error while running migration: %v", err)
+		}
+	case "down":
+		fmt.Println("undoing migration....!")
+		err = m.Down()
+		if err != nil {
+			log.Fatalf("error while running migration: %v", err)
+		}
+	default:
+		log.Fatal("please provide migration name <up | down>")
+		return
+	}
+
+}
